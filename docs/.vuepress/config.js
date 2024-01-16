ab7e5e35a7a2cd5ddc41bfc5d983e0e0cd02e353
Florian <1technophile@users.noreply.github.com>
2024-01-16 08:40:05 -0600

    [DOCS] Add sitemap plugin

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,5 +5,22 @@ module.exports = {
   themeConfig: {
     repo: 'theengs',
     ...commonConfig.themeConfig,
+  },
+  plugins: {
+    'sitemap': {
+      hostname: 'https://www.theengs.io',
+      urls: [
+        'https://decoder.theengs.io/devices/devices.html',
+        'https://community.openmqttgateway.com/',
+        'https://shop.theengs.io/',
+        'https://shop.theengs.io/products/theengs-plug-smart-plug-ble-gateway-and-energy-consumption',
+        'https://shop.theengs.io/products/theengs-bridge-esp32-ble-mqtt-gateway-with-ethernet-and-external-antenna',
+        'https://app.theengs.io/',
+        'https://gateway.theengs.io/',
+        'https://decoder.theengs.io/',
+        'https://parser.theengs.io/',
+        'https://docs.openmqttgateway.com/'
+      ],
+    },
   }
 }
